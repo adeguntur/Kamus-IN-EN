33a45209385b7f94fc20ae71ab6c025dfaba997f
ade <adegunutur2001@gmail.com>
2018-03-01 14:57:37 +0700

    Initial commit

---
+++ app/src/main/java/com/guntur/ade/kamusin_en/DetailActivity.java
@@ -0,0 +1,32 @@
+
+package com.guntur.ade.kamusin_en;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class DetailActivity extends AppCompatActivity {
+
+    public static final String ITEM_WORD = "ITEM_WORD";
+    public static final String ITEM_TRANSLATE = "ITEM_TRANSLATE";
+
+    @BindView(R.id.tv_word)
+    TextView tv_word;
+
+    @BindView(R.id.tv_translate)
+    TextView tv_translate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        ButterKnife.bind(this);
+
+        tv_word.setText(getIntent().getStringExtra(ITEM_WORD));
+        tv_translate.setText(getIntent().getStringExtra(ITEM_TRANSLATE));
+    }
+}
